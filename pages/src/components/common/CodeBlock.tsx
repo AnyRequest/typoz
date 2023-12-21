2dde51c529c969bf11d570c256b5bee887642d9e
kkn1125 <xpfjrnseks1@naver.com>
2023-12-21 13:46:37 +0900

    ✨ feat: 랜딩페이지 react 사용 예시 추가, typoz 사용 컴포넌트 예시 추가

--- pages/src/components/common/CodeBlock.tsx
+++ pages/src/components/common/CodeBlock.tsx
@@ -3,6 +3,7 @@ import IntegrationInstructionsIcon from '@mui/icons-material/IntegrationInstruct
 import {
   Box,
   IconButton,
+  Stack,
   SxProps,
   Theme,
   Tooltip,
@@ -13,17 +14,30 @@ import { useEffect, useState } from 'react';
 import Prism from 'prismjs';
 
 type CodeBlockProps = {
+  filename?: string;
   language: string;
   code: string;
+  folding?: boolean;
   sx?: SxProps<Theme>;
 };
 
-function CodeBlock({ language = 'javascript', code, sx = {} }: CodeBlockProps) {
+function CodeBlock({
+  filename,
+  language = 'javascript',
+  code,
+  folding = false,
+  sx = {},
+}: CodeBlockProps) {
   const [copied, setCopied] = useState(false);
+  const [toggleFolding, setToggleFolding] = useState(false);
 
   useEffect(() => {
     Prism.highlightAll();
   }, []);
+
+  function handleToggleFolding() {
+    setToggleFolding(!toggleFolding);
+  }
 
   function copyToClipboard() {
     navigator.clipboard.writeText(code);
@@ -36,24 +50,69 @@ function CodeBlock({ language = 'javascript', code, sx = {} }: CodeBlockProps) {
   return (
     <Box
       sx={{
+        transition: '150ms ease-in-out',
+        overflow: 'hidden',
         position: 'relative',
         my: 2,
-        borderRadius: '0.3em !important',
-        overflow: 'hidden',
+        borderRadius: '0.2em !important',
+        '&::-webkit-scrollbar': {
+          width: 5,
+          height: 5,
+          backgroundColor: 'inherit',
+        },
+        '&::-webkit-scrollbar-thumb': {
+          width: 5,
+          height: 5,
+          backgroundColor: 'inherit',
+        },
+        '&::-webkit-scrollbar-corner': {
+          width: 5,
+          height: 5,
+          backgroundColor: 'inherit',
+        },
       }}
     >
-      <Typography
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
         sx={{
           color: 'background.default',
           backgroundColor: 'text.secondary',
           px: 2,
           py: 1,
+          minHeight: 50,
         }}
-        textTransform={'uppercase'}
       >
-        {language}
-      </Typography>
-      <Box component="div">
+        <Typography textTransform={filename ? 'unset' : 'uppercase'}>
+          {filename || language}
+        </Typography>
+        {folding && (
+          <Tooltip title={toggleFolding ? '접기' : '열기'} placement="top">
+            <IconButton size="small" onClick={handleToggleFolding}>
+              {toggleFolding ? '📁' : '📂'}
+            </IconButton>
+          </Tooltip>
+        )}
+      </Stack>
+      <Box
+        component="div"
+        sx={{
+          ...(folding && {
+            pre: {
+              transition: '150ms ease-in-out',
+              ...(!toggleFolding
+                ? {
+                    filter: 'blur(2px)',
+                    maxHeight: '50px',
+                  }
+                : {
+                    maxHeight: '100vh',
+                  }),
+            },
+          }),
+        }}
+      >
         <Box
           component="pre"
           sx={{
@@ -93,7 +152,7 @@ function CodeBlock({ language = 'javascript', code, sx = {} }: CodeBlockProps) {
           onClick={copyToClipboard}
           sx={{
             position: 'absolute',
-            top: 48,
+            top: 56,
             right: 8,
             [copied ? 'color' : 'background']: (theme) =>
               copied
