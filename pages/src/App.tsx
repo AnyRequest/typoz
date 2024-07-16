50c230cfaffebfc7a4422b2eb1bc1c89dc2c2c6d
kkn1125 <xpfjrnseks1@naver.com>
2024-07-17 01:02:05 +0900

    🛠️ fix: gh-pages navigate route 오류 수정

--- pages/src/App.tsx
+++ pages/src/App.tsx
@@ -12,8 +12,8 @@ function App() {
       <Route path="" element={<Layout />}>
         <Route path={BASE_PATH} element={<Landing />} />
         {/* <Route path={BASE_PATH + 'test/'} element={<Test />} /> */}
-        <Route path="document" element={<Docs />} />
-        <Route path="advanced" element={<Advanced />} />
+        <Route path={BASE_PATH + 'document'} element={<Docs />} />
+        <Route path={BASE_PATH + 'advanced'} element={<Advanced />} />
       </Route>
       {/* <Route path={BASE_PATH + 'api'}>
         <Route path="render" element={<ApiComponent />} />
